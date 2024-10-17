6d46066d6a65475e4a6cc333a8dc9efeb69c0b34
artem <shuranov16@bk.ru>
2024-10-17 12:41:43 +0300

    Программа проходит все тесты

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,43 @@
-#include <assert.h>
 #include "int2str.h"
 #include "stdio.h"
+#include "stdbool.h" 
+ 
+char result[12] = ""; 
+ 
+bool sign(int n) { 
+    return (n < 0); 
+} 
+ 
+char* int2str(int number){ 
+ 
+    char* resultl = result; 
 
-char* int2str(int number) {
-    char* str = "0";
-    return str;
+    if (number == 2147483647) {
+        return "2147483647";
+    } 
+
+    if (number == -2147483648) {
+        return "-2147483648";
+    } 
+
+    if (sign(number)) { 
+        *resultl++ = '-'; 
+        number *= -1; 
+    } 
+ 
+    int shifter = number; 
+ 
+    do { 
+        ++resultl; 
+        shifter /= 10; 
+    }while(shifter); 
+ 
+    *resultl = '\0'; 
+ 
+    do {  
+        *--resultl = number % 10 + '0';  
+        number /= 10; 
+    }while(number); 
+ 
+    return result; 
 }
